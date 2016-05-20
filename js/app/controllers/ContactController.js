8161bff6351792d3e725d530d6c118b299272aab
authorbeard <msnolan@gmail.com>
2016-05-20 09:32:03 -0400

    all specs passing

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,17 +1,26 @@
 function ContactController() {
     this.contacts = [{
         name: 'Bob',
-        phone: '0123458690'
+        phoneNumber: '0123458690'
     },{
         name: 'Tim',
-        phone: '3934203242'
+        phoneNumber: '3934203242'
     },{
         name: 'Ross',
-        phone: '0684059433'
+        phoneNumber: '0684059433'
     }];
 
     this.removeContact = function (index) {
         this.contacts.splice(index, 1);
+    }
+
+    this.addContact = function(name, phone){
+        // debugger;
+        var contact = {
+            name: name,
+            phoneNumber: phone
+        }
+        this.contacts.push(contact)
     }
 }
 
